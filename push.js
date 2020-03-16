1fbba59ce2e0989918f1c7901a34a94547f83444
Korakod Chaisuwan <korakod.c@g-able.com>
2020-03-16 15:09:25 +0700

    fix

--- push.js
+++ push.js
@@ -7,13 +7,9 @@ let vapidKeys = {
 
   push.setVapidDetails('mailto.test@code.co.th', vapidKeys.publicKey, vapidKeys.privateKey)
 
-  let sub ={
-      endpoint:
-        "https://fcm.googleapis.com/fcm/send/czhjfjMr-Cc:APA91bG6l7QpP2oHjNXc0gxBkMc0kJrYjh1ph02r8nBCXgZaA56TwwXrHipvpSiF6JAWdgaZElSZNGK2zncDS39NfAddkS5aZb4n8OBFz5Ah-MVWThobGTR76h5V1n847ngKktprr8IB",
-      expirationTime: null,
-      keys:{
-          "p256dh":"BHDOhv9Q_QRd2K-gKhxnc-clJpY6T5DVytJpQk1LoKyFs1UUexashrSDG51hFCuN2HOdLU4VLJ3BvY33zgymcjQ",
-      auth:
-      "zHif4ttqSgQukQiW6YA-xw"
-    }};
+  let sub = {endpoint:"https://fcm.googleapis.com/fcm/send/czhjfjMr-Cc:APA91bG6l7QpP2oHjNXc0gxBkMc0kJrYjh1ph02r8nBCXgZaA56TwwXrHipvpSiF6JAWdgaZElSZNGK2zncDS39NfAddkS5aZb4n8OBFz5Ah-MVWThobGTR76h5V1n847ngKktprr8IB",
+  expirationTime: null,
+  keys: {"p256dh":"BHDOhv9Q_QRd2K-gKhxnc-clJpY6T5DVytJpQk1LoKyFs1UUexashrSDG51hFCuN2HOdLU4VLJ3BvY33zgymcjQ",
+  auth: "zHif4ttqSgQukQiW6YA-xw"
+}};
   push.sendNotification(sub, 'test message')
